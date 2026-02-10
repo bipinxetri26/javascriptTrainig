688c22c64a7cc44b4a4eac8d22e3fb1e4779f918
bipinxetri26 <bipinxetri26@gmail.com>
2026-02-10 10:12:12 +0545

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// var , let and const in js
+var a = 10;
+var a = 20;
+console.log(a)
+
+
+// var is global scoped that can be re-declared
+// let is block scoped variable that cannot be re-declared.
+// let b= 20;
+// let b=30;
