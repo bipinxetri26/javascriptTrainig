14d2ad8e19e552cd37538734ccdac7464e07530e
bipinxetri26 <bipinxetri26@gmail.com>
2026-02-17 10:22:55 +0545

    initial commit

---
+++ day7/array.js
@@ -0,0 +1,18 @@
+const arr = [1,2,3,"hello ","jay",3.34]
+// console.log(arr[0])
+// console.log(arr[5])
+// console.log(arr.length)  //show how many value are there
+//Array method
+console.log(arr.sort())  //arrange ascending order
+//push mehtod add element at the last index of array
+// console.log(arr.push("sushant")) 
+// console.log(arr.pop())  //exit last word or letter 
+// console.log(arr.shift(3.5))
+// arr.unshift("ramlal")  // put value or word in 1st
+console.log(arr.at(4))
+console.log(arr);
+
+
+// for (let i=0;i<arr.length;i++){
+//     console.log(arr[i])
+// }
--- note.js
+++ note.js
@@ -1,15 +1,15 @@
 //Q1. subtraction by busing function
 
-// function sub(a,b){
-//     return a-b 
-// }
-// console.log(sub(12,9))
+function sub(a,b){
+    return a-b 
+}
+console.log(sub(12,9))
 
 //Q2. arrow function that divide 2 numbers
 const div=(x,y) =>x/y;
 console.log(div(20,4))
 
-//
+
 // const number = (a,b)=>{
 // if(a>b){
 //     console.log(a + " greater number")
@@ -23,6 +23,13 @@ console.log(div(20,4))
 //Q5. arrow function that return the greatest of 2 number
 const greatest = (a,b)=>(a>b?a:b)
 console.log(greatest(67,89))
+
+//Q7.  function that return the product of 3 numbers.
+function threenumber(p,q,r){
+    return(p*q*r)
+}
+console.log(threenumber(12, 2, 4))
+
 
 //Q8. check the number is even or odd using arrow function
 const number = (a)=>{
